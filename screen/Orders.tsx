1aa761a6dd55bab2c2aa4c7c55e9ecc1c7d48acb
kabottin0 <gc.eforge@gmail.com>
2024-02-15 10:59:42 +0100

    order and terminalino screen

--- screen/Orders.tsx
+++ screen/Orders.tsx
@@ -3,15 +3,35 @@ import { Text, View } from "native-base"
 
 const Orders = () => {
 
+  //ToDo tabella operazione "ordini ha più operazioni", status "inviato" "bozza", lista prodotti selezionati
+  const requestProductsList = () => { }
+  const saveProductsList = () => {
+    //ToDo query per il salvataggio nel nostro db dell'operazione con lo status e la lista prodotti
+  }
+  const getProductlist = () => {
+    //ToDo query per il recupero del record se bozza o inviato
+  }
 
-    return(
-        <>
-        <View justifyContent={'center'} alignItems={'center'}>
-          <Text> Pagina degli ordini</Text>
-         
+  const resumeAndSend = () =>{
+    //ToDo riassuno del carrello in json per l'ivio della ricevuta al backand
+  }
+const print = () => {
+  //toDo funzione stampa per la termical printer
+}
+  return (
+    <>
+      <View justifyContent={'center'} alignItems={'center'}>
+        <Text> Pagina degli ordini</Text>
+        <View>
+          <Text>
+            renderizzazione lista dei prodotti - componente prodotto selezionabile ed inserimento in un "carrello"
+          </Text>
         </View>
-      </>
-    )
+        <Text>Storico ricevute</Text>
+        <Text>Stampa ricevuta</Text>
+      </View>
+    </>
+  )
 }
 
 export default Orders
--- screen/Terminalino.tsx
+++ screen/Terminalino.tsx
@@ -1,17 +1,46 @@
-import { Text, View } from "native-base"
+import { Button, Text, View } from "native-base"
+import { useState } from "react"
+import { SafeAreaView } from "react-native"
 
 
 const Terminalino = () => {
+  const [barCodeValue, setBarCodeValue] = useState()
+  const barCodeFunct = () => {
+    //ToDo funzione per catturare l'ean del prodotto tramite bar code
+  }
+  const requestDetailsProduct = () => {
+    //ToDo richiesta api per i dettagli del proddoto filtrato per ean scansionato
+  }
+  const updateProduct = () => {
+    //ToDo salva modifica prodotto nel db locale e invio tramite api al backend
+  }
 
 
-    return(
-        <>
+
+
+  return (
+    <>
+      <SafeAreaView>
         <View justifyContent={'center'} alignItems={'center'}>
           <Text> Pagina del bar code</Text>
-         
+          <View justifyContent={'center'} alignItems={'center'}>
+            <Button>
+              Read bar code
+            </Button>
+            <View>
+              <Text>
+                renderizziamo i dettagli ricevuti-componente per la renderizzazione dettagli
+              </Text>
+            </View>
+            <Button>
+              Conferma le modifiche e invia
+            </Button>
+          </View>
         </View>
-      </>
-    )
+      </SafeAreaView>
+    </>
+  )
 }
 
 export default Terminalino
+
